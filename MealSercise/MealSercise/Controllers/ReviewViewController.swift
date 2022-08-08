3de68bada1a0ab1d5731865c548734ca40d6cda6
HuidanKuang <kuanghuidan@gmail.com>
2022-08-07 17:02:38 -0700

    Review UI continue and control the rows of the review table by the stepper

--- MealSercise/MealSercise/Controllers/ReviewViewController.swift
+++ MealSercise/MealSercise/Controllers/ReviewViewController.swift
@@ -7,25 +7,71 @@
 
 import UIKit
 
-class ReviewViewController: UIViewController {
+class ReviewViewController: UIViewController,UITableViewDataSource, UITableViewDelegate {
 
     @IBOutlet weak var reviewTable: UITableView!
-    override func viewDidLoad() {
-        
-        super.viewDidLoad()
+    
+    @IBOutlet weak var labelPlanNum: UILabel!
+    
+    
+    @IBOutlet weak var labelMax: UILabel!
+    
+    
+    @IBOutlet weak var StepperMaxValue: UIStepper!
 
-        // Do any additional setup after loading the view.
+    struct dietPlan {
+        let breakfast: String
+        let lunch: String
+        let dinner: String
+        let exercise: String
     }
     
+    var dietData:[dietPlan] = [
+        dietPlan(breakfast: "vege", lunch: "vege", dinner: "vege", exercise: "running"),
+        dietPlan(breakfast: "sala", lunch: "vege", dinner: "vege", exercise: "running"),
+    ]
+    
+    override func viewDidLoad() {
+        labelPlanNum.text=String(dietData.count)
+        
+        labelMax.text="0"
+        
+        super.viewDidLoad()
+        reviewTable.dataSource = self
+        reviewTable.delegate = self
+        
+        StepperMaxValue.minimumValue = 0
+        StepperMaxValue.maximumValue = Double(dietData.count)
+        
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
     }
-    */
+    
+    
+    @IBAction func stepperMax(_ sender: UIStepper) {
+        
+        labelMax.text=sender.value.description
+        
+        reviewTable.reloadData()
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return Int(StepperMaxValue.value)  }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let dietPlan = dietData[indexPath.row]
+        let reviewCell = reviewTable.dequeueReusableCell(withIdentifier: "reviewCell", for: indexPath) as! ReviewTableViewCell
+        reviewCell.labelBreakfast.text = dietPlan.breakfast
+        reviewCell.labelLunch.text = dietPlan.lunch
+        reviewCell.labelDinner.text = dietPlan.dinner
+        reviewCell.labelEx.text = dietPlan.exercise
+        return reviewCell
+    }
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 127
+    }
+    
+    
+
 
 }
